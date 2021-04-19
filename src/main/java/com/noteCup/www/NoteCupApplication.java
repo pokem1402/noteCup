e2d34431ba202baa752bc5727d93c53ea55dca86
unknown <dlalstjd74@naver.com>
2021-04-19 15:15:56 +0900

    hello

--- src/main/java/com/noteCup/www/NoteCupApplication.java
+++ src/main/java/com/noteCup/www/NoteCupApplication.java
@@ -8,6 +8,7 @@ public class NoteCupApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(NoteCupApplication.class, args);
+		System.out.println("hello world");
 	}
 
 }
